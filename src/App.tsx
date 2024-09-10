5ffe97bf03285da3663d3e2d5b80acd574e09b16
hide-alzheimer <polterskii@gmail.com>
2024-09-11 01:11:41 +0700

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex p-10 w-100 border-4 black gap-5 font-caveat">
+        <div className="w-1/2">         
+          <div className="border-4 black">picture</div>
+          <div className="my-10 border-4 black">Aboutme
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </div>
+          <div className="my-10 border-4 black">education
+            <li></li>
+            <li></li>
+          </div>
+          <div className="my-10 border-4 black">experience
+            <li></li>
+            <li></li>
+            <li></li>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <div className="">Job position
+            <p>Front-end Developer</p>
+          </div>
+          <div className="my-10 border-4 black">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum cumque earum dignissimos nihil doloremque error ducimus corporis tenetur illo aut non, quod dolorem nobis vel ipsam, nam exercitationem. Quo, consequuntur.</p>
+          </div>
+          <div className="my-10 border-4 black">skill</div>
+          <div className="my-10 border-4 black">expertise</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
